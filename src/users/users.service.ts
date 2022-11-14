f76aab5941e5cf1039f67c597f90bae45417d9e8
MarkGoren <gormark2001@gmail.com>
2022-11-14 11:43:38 +0200

    added register

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'src/typeorm/enteties/users.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { response, Response } from 'express';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users) private usersRepository: Repository<Users>,
   ) {}
+
+  userExists(email) {
+    return this.usersRepository.query(
+      `select * from users where email like '${email}'`,
+    );
+  }
+
+  userRegister(userInfo) {
+    const username = userInfo.email.split('@')[0];
+    const hashed = bcrypt.hashSync(userInfo.password, 10);
+    this.usersRepository.query(
+      `insert into users (username, email, password_hash, is_sub) values ('${username}', '${userInfo.email}', '${hashed}', false)`,
+    );
+  }
 }

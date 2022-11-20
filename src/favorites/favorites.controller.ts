d5f85599d2254e24956708c86615580b6ad1ceaf
MarkGoren <gormark2001@gmail.com>
2022-11-20 16:09:49 +0200

    added favorites

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import { info } from 'console';
+import { FavoritesService } from './favorites.service';
 
 @Controller('favorites')
-export class FavoritesController {}
+export class FavoritesController {
+  constructor(private readonly favoritesService: FavoritesService) {}
+  @Get('getAll/:userId')
+  getAll(@Param('userId') userId: number) {
+    return this.favoritesService.getAll(userId);
+  }
+
+  @Get('getPlaylists/:userId')
+  getPlaylists(@Param('userId') userId: number) {
+    return this.favoritesService.getPlaylists(userId);
+  }
+
+  @Post('add')
+  add(
+    @Body()
+    info,
+    @Res() res,
+  ) {
+    this.favoritesService.add(info.userId, info.playlistId);
+    res.end();
+  }
+
+  @Post('remove')
+  remove(
+    @Body()
+    info,
+    @Res() res,
+  ) {
+    this.favoritesService.remove(info.userId, info.playlistId);
+    res.end();
+  }
+}
--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -9,4 +9,28 @@ export class FavoritesService {
     @InjectRepository(Favorites)
     private favoritesRepository: Repository<Favorites>,
   ) {}
+
+  getAll(userId) {
+    return this.favoritesRepository
+      .query(`SELECT playlist_id FROM favorites where user_id = ${userId};`)
+      .then((data) => data.map((obj) => obj.playlist_id));
+  }
+
+  getPlaylists(userId) {
+    return this.favoritesRepository.query(
+      `SELECT * FROM playlists where id in (SELECT playlist_id FROM blotter_data_demo.favorites where user_id = ${userId});`,
+    );
+  }
+
+  remove(userId, playlistId) {
+    this.favoritesRepository.query(
+      `delete from favorites where user_id = ${userId} and playlist_id = ${playlistId}`,
+    );
+  }
+
+  add(userId, playlistId) {
+    this.favoritesRepository.query(
+      `insert into favorites (user_id, playlist_id) values (${userId}, ${playlistId})`,
+    );
+  }
 }
